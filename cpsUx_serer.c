50e62a1ba439f041a96804c8c1e189a2f7e7d01c
Richard Liu <richard08002003@hotmail.com>
2015-02-06 10:34:45 +0800

    Update cpsUx_serer.c
    New version_0204

--- cpsUx_serer.c
+++ cpsUx_serer.c
@@ -12,14 +12,14 @@
 
 #define DF_CPS_UNSKT_FILE_PATH         "/tmp/cpsUxSkt.ipc"
 
-FILE *fp1 , *fp2 , *fp3 , *fp4  , *fp5 ;
+FILE *fp1 , *fp2 , *fp3 , *fp4 ;
 int main(void) {
     struct sockaddr_un server , client ;
     /**Create a socket*/
     int sock = socket( AF_UNIX , SOCK_STREAM , 0 ) ;
     if ( sock < 0) {
         perror("socket") ;
-        exit(1) ;
+        exit(1);
     }
     printf("Socket Created!\n") ;
 
@@ -27,12 +27,14 @@ int main(void) {
     strcpy(server.sun_path , DF_CPS_UNSKT_FILE_PATH) ;
     unlink(server.sun_path) ;
     int len = strlen(server.sun_path) + sizeof(server.sun_family) ;
+    int on = 1 ;
+    setsockopt(sock,SOL_SOCKET,SO_REUSEADDR,&on,sizeof(on)) ;
     /**bind() */
     if ( bind(sock , (struct sockaddr *)&server , len) < 0 ) {
         perror("bind") ;
         exit(1) ;
     }
-        printf("bind successed!\n") ;
+    printf("bind successed!\n") ;
 
     /**listen() */
     if ( listen( sock , 10 ) < 0 ) {
@@ -41,7 +43,7 @@ int main(void) {
     }
         printf("listen successed!\n") ;
 
-    while(1) {
+    //while(1) {
         printf("Waiting for connection . . . \n") ;
         int clen = sizeof(client) ;
         /**accept() 接受client 端的連線要求*/
@@ -61,18 +63,9 @@ int main(void) {
         } else if ( fpid == 0 ) {
             /**child process
            **send()  寫資料*/
-            while(1) {
-                while(1) {
-                    printf("請輸入欲傳送的檔案名稱：") ;
-                    char file1[100] = {0} ;
-                    fgets( file1 , sizeof(file1) , stdin ) ;
-                    char *p =strrchr( file1 , '\n' ) ;
-                    if ( p != NULL) {
-                        *p = 0 ;
-                    }
-                    printf("%s" , file1) ;
-                    fp1 = fopen( file1 , "r" ) ; //開啟檔案data.txt
-                    fp2 = fopen( "parsedata.txt" , "w" ) ;//開啟檔案result.txt
+                    printf("開啟 %s 檔案\n" , "data1.txt") ;
+                    fp1 = fopen( "data1.txt" , "r" ) ; //開啟檔案data.txt
+                    fp2 = fopen( "parsedata1.txt" , "w" ) ;//開啟檔案result.txt
                     if ( (fp1 == NULL) || (fp2 == NULL) ) {
                         printf("檔案開啟錯誤！\n") ;
                         exit(1) ;
@@ -97,9 +90,11 @@ int main(void) {
                         fprintf( fp2 , "%c" , c ) ;
                         }
                     }
+                    printf("%s 分析完畢 . . . \n","data1.txt") ;
                     fclose(fp1) ;
                     fclose(fp2) ;
-                    fp3 = fopen( "parsedata.txt" , "r" )  ; //開啟parsedata.txt
+
+                    fp3 = fopen( "parsedata1.txt" , "r" )  ; //開啟parsedata.txt
                     fp4 = fopen( "Cps_SendLog.txt" , "w" ) ;
                     if ( (fp3 ==NULL) || (fp4 ==NULL) ) {
                         printf("檔案開啟錯誤！\n") ;
@@ -115,30 +110,7 @@ int main(void) {
                         fprintf( fp4 , "%s" , "傳送的資料為：" )  ;
                         fprintf( fp4 , "%s\n" , str ) ;
 
-                        /**將所得到的JSON資料訊息壓成MD5編碼*/
-                        MD5_CTX ctx ;
-                        //MD5_Init ()
-                        int check = MD5_Init(&ctx) ;
-                        if ( check == 0 ) {
-                            printf("MD5_Init Failed ! \n") ;
-                            fprintf( fp4 , "%s" , "MD5_Init Failed ! \n" ) ;
-                            exit(1) ;
-                        }
-                        //MD5_Update() 將要編碼的資料放到MD5_CTX *c 內
-                        MD5_Update(&ctx , str , strlen(str) ) ;
-                        unsigned char md5code[16] = {0} ;
-                        MD5_Final( md5code , &ctx ) ;
-                        int i ;
-                        printf("MD5編碼為 : ") ;
-                        fprintf( fp4 , "%s" , "MD5編碼為 :" ) ;
-                        for ( i = 0 ; i < 16 ; i++ ) {
-                            printf( "%02X" , md5code[i] ) ;
-                            fprintf( fp4 , "%02X" , md5code[i] ) ;
-                        }
-                        printf("\n") ;
-                        fprintf( fp4 , "%s" , "\n" ) ;
-
-                        //send() 將資料寄給MainGateway
+                        //send() 將一筆一筆資料傳給MainGateway
                         if( send( sock2 , str , strlen(str) , 0) < 0 ) {
                             printf("Send Failed ! \n") ;
                             fprintf( fp4 , "%s" , "Send Failed ! \n" ) ;
@@ -150,62 +122,27 @@ int main(void) {
                     }
                     fclose(fp3) ;
                     fclose(fp4) ;
-                }
-            }
 
         } else {
             /**parent process
-            ** recv() 讀取資料*/
+            **recv() 讀取資料*/
                 while(1) {
                     //初始化str[ ]
                     unsigned char str[1000] ={0} ;
-                    fp5 = fopen( "Cps_ReceiveLog.txt" , "w" ) ;
-                    if ( fp5 == NULL ) {
-                        printf("檔案開啟錯誤 ! \n") ;
-                        exit(1) ;
-                    }
                     //recv() 接收來自maingateway client 端的信息
                     int val = recv( sock2 , str , sizeof(str) , 0) ;
                     if( val < 0 ) {
                         printf("\nReceive messages Failed!! \n") ;
-                        fprintf( fp5 , "%s" , "\nReceive messages Failed!! \n" ) ;
                         break ;
                     } else if ( val == 0 ) {
                         printf("\nDisconnected to Maingateway client . . . \n") ;
-                        fprintf( fp5 , "%s" , "\nDisconnected to Maingateway client . . . \n" ) ;
                         break ;
                     } else {
-                        printf("\n接收 Client's 的資料為：%s \n", str ) ;
-                        fprintf( fp5 , "%s" , "\n接收 Client's 的資料為：" ) ;
-                        fprintf( fp5 , "%s\n" , str ) ;
-
-                        /** 將收到的資料壓成MD5 */
-                        MD5_CTX ctx ;
-                        int check = MD5_Init(&ctx) ;
-                        if ( check == 0 ) {
-                            printf("MD5_Init Failed ! \n") ;
-                            fprintf( fp5 , "%s" , "MD5_Init Failed ! \n" ) ;
-                            exit(1) ;
-                        }
-                         //MD5_Update() 將要編碼的資料放到MD5_CTX *c 內
-                        MD5_Update(&ctx , str , strlen(str) ) ;
-                        unsigned char md5code[16] = {0} ;
-                        MD5_Final( md5code , &ctx ) ;
-                        int i ;
-                        printf("MD5編碼為 : ") ;
-                        fprintf( fp5 , "%s" , "MD5編碼為 : " ) ;
-                        for ( i = 0 ; i < 16 ; i++ ) {
-                            printf("%02X" , md5code[i] ) ;
-                            fprintf( fp5 , "%02X" , md5code[i] ) ;
-                        }
-                        printf("\n") ;
-                        fprintf( fp5 , "%s" , "\n\n" ) ;
+                        printf("\n接收 Client 的資料為 --> %s \n", str ) ;
                     }
                 }
         }
         close(sock2) ;
-        fclose(fp5) ;
-    }
     close(sock) ;
     return 0 ;
 }
